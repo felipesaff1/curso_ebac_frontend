e033897d53875f9c6c58af77a14981189c3e9e1b
Felipe Andrade <felipe_saff1@hotmail.com>
2025-04-09 21:04:17 -0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('form-sorteador').addEventListener('submit', function(evento){
         evento.preventDefault();
-        let numeroMaximo= document.getElementById('numero-maximo').value;
-        numeroMaximo = parseInt(numeroMaximo);
-
-        let numeroAleatorio= Math.random() * numeroMaximo;
-        numeroAleatorio= Math.floor(numeroAleatorio+1);
-        //alert(numeroAleatorio);
-        document.getElementById('resultado-valor').innerText= numeroAleatorio;
+        let num1 = parseFloat(document.getElementById('numerador').value);
+        let num2 = parseFloat(document.getElementById('denominador').value);
+        let operacao = document.getElementById('operacao').value;
+        switch (operacao) {
+            case "soma":
+                resultado = num1 + num2;
+                break;
+            case "subtracao":
+                resultado = num1 - num2;
+                break;
+            case "multiplicacao":
+                resultado = num1 * num2;
+                break;
+            case "divisao":
+                resultado = num2 !== 0 ? num1 / num2 : "Erro (divisão por zero)";
+                break;
+            default:
+                resultado = "Operação inválida";
+        }
+        //alert(resultado);
+        document.getElementById('resultado-valor').innerText= resultado;
         document.querySelector('.resultado').style.display='block';
     })
 })
